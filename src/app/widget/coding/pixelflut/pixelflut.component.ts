36003da26cb77efed32ac746143988ae42b7fcf0
Finn Sell <dev@finn-thorben.me>
2019-01-06 18:50:43 +0100

    Implement pixelflut scaling

--- src/app/widget/coding/pixelflut/pixelflut.component.ts
+++ src/app/widget/coding/pixelflut/pixelflut.component.ts
@@ -1,89 +1,91 @@
-import {Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {Observable, Observer} from "rxjs";
 
 @Component({
-  selector: 'app-pixelflut',
-  templateUrl: './pixelflut.component.html',
-  styleUrls: ['./pixelflut.component.css']
+    selector: 'app-pixelflut',
+    templateUrl: './pixelflut.component.html',
+    styleUrls: ['./pixelflut.component.css']
 })
-export class PixelflutComponent implements OnInit, OnDestroy {
+export class PixelflutComponent implements OnInit {
+    public canvasXSize = 600;
+    public canvasYSize = 300;
 
-  public initialized = false;
-  public xSize;
-  public ySize;
+    private xSize: number;
+    private ySize: number;
+    private xScaling: number;
+    private yScaling: number;
 
-  private canvas;
-  private obs: Observable<MessageEvent>;
+    private canvasCtx;
+    private obs: Observable<MessageEvent>;
 
-  constructor(private elRef: ElementRef) { }
+    constructor(private elRef: ElementRef) {}
 
-  ngOnInit() {
-    let _this = this;
+    ngOnInit() {
+        let _this = this;
 
-    this.obs = this.connect("wss://finn-thorben.me/pixelflut/", "pixelflut-websocket");
+        this.obs = this.connect("wss://finn-thorben.me/pixelflut/", "pixelflut-websocket");
 
-    this.obs.subscribe(function (event) {
-      _this.updateHandler(event);
-    }, null, null);
+        this.obs.subscribe(function (event) {
+            _this.updateHandler(event);
+        }, null, null);
 
-    console.log("Connected to pixelflut server");
-  }
-
-  private connect(url: string, protocol: string): Observable<MessageEvent> {
-    let sock = new WebSocket(url, protocol);
-
-    return Observable.create((obs: Observer<MessageEvent>) => {
-      sock.onmessage = obs.next.bind(obs);
-      sock.onerror = obs.error.bind(obs);
-      sock.onclose = obs.complete.bind(obs);
-      return sock.close.bind(sock);
-    });
-  }
-
-  private updateHandler(event: MessageEvent) {
-    this.getCanvas();
-    let msgs = event.data.toString().split(";");
-    for (let msg of msgs) {
-      if (msg.startsWith("SIZE")) {
-        this.handle_SIZE(msg);
-      }
-      else if (msg.startsWith("PX")) {
-        this.handle_PX(msg);
-      }
-      else {
-        //console.error("Unknown pixelflut message: " + msg);
-      }
+        console.log("Connected to pixelflut server");
     }
-  }
 
-  getCanvas() {
-    if (this.canvas == undefined) {
-      this.canvas = this.elRef.nativeElement.getElementsByTagName("canvas")[0];
+    private connect(url: string, protocol: string): Observable<MessageEvent> {
+        let sock = new WebSocket(url, protocol);
+
+        return Observable.create((obs: Observer<MessageEvent>) => {
+            sock.onmessage = obs.next.bind(obs);
+            sock.onerror = obs.error.bind(obs);
+            sock.onclose = obs.complete.bind(obs);
+            return sock.close.bind(sock);
+        });
     }
-  }
 
-  handle_SIZE(msg: String) {
-    let splitted = msg.split(" ");
-    this.xSize = splitted[1];
-    this.ySize = splitted[2];
+    private updateHandler(event: MessageEvent) {
+        if (this.canvasCtx == undefined) {
+            this.getCanvas();
 
-    this.initialized = true;
-  }
-
-  handle_PX(msg: String) {
-    if (this.canvas != undefined) {
-      let splitted = msg.split(" ");
-      let x = splitted[1];
-      let y = splitted[2];
-      let color = splitted[3];
-
-      this.canvas.getContext("2d").fillStyle = "#" + color;
-      this.canvas.getContext("2d").fillRect(x, y, 1, 1);
+        } else {
+            let msgs = event.data.toString().split(";");
+            for (let msg of msgs) {
+                if (msg === "") {}
+                else if (msg.startsWith("SIZE")) {
+                    this.handle_SIZE(msg);
+                } else if (msg.startsWith("PX")) {
+                    this.handle_PX(msg);
+                } else {
+                    console.error("Unknown pixelflut message: " + msg);
+                }
+            }
+        }
     }
-  }
 
-  ngOnDestroy(): void {
+    getCanvas() {
+        this.canvasCtx = this.elRef.nativeElement.getElementsByTagName("canvas")[0].getContext("2d");
+    }
 
-  }
+    handle_SIZE(msg: String) {
+        let slitted = msg.split(" ");
+        this.xSize = +slitted[1];
+        this.ySize = +slitted[2];
+        this.xScaling = this.canvasXSize / this.xSize;
+        this.yScaling = this.canvasYSize / this.ySize;
+    }
+
+    handle_PX(msg: String) {
+        let splitted = msg.split(" ");
+        let x = +splitted[1];
+        let y = +splitted[2];
+        let color = splitted[3];
+
+        this.canvasCtx.fillStyle = "#" + color;
+        this.canvasCtx.fillRect(
+            x * this.xScaling,
+            y * this.yScaling,
+            this.xScaling,
+            this.yScaling);
+    }
 
 }
