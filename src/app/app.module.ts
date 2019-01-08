37fb7fb536465aa6f3e42352c46669298f2c72ec
Finn Sell <dev@finn-thorben.me>
2019-01-08 23:07:23 +0100

    Rewrite most components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import {DesktopComponent} from './desktop/desktop.component';
 import {TaskbarComponent} from './taskbar/taskbar.component';
 import {WidgetComponent} from './widget/widget.component';
 import {StartMenuComponent} from './taskbar/start-menu/start-menu.component';
+import {ContactComponent} from './widget/contents/contact/contact.component';
+import {CodingComponent} from './widget/contents/coding/coding.component';
+import {CvComponent} from './widget/contents/cv/cv.component';
+import {ImprintComponent} from './widget/contents/imprint/imprint.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,10 @@ import {StartMenuComponent} from './taskbar/start-menu/start-menu.component';
     TaskbarComponent,
     WidgetComponent,
     StartMenuComponent,
+    ContactComponent,
+    CodingComponent,
+    CvComponent,
+    ImprintComponent,
   ],
   imports: [
     BrowserModule,
