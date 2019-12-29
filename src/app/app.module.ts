9f8f360aedaea0b0cae1b3f2f421b1a25a865f3b
Finn-Thorben Sell <dev@finn-thorben.me>
2019-12-29 13:26:43 +0100

    update pixelflut client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AngularFontAwesomeModule} from "angular-font-awesome";
-import {AngularDraggableModule} from "angular2-draggable";
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {AngularDraggableModule} from 'angular2-draggable';
 
 import {AppComponent} from './app.component';
 import {DesktopComponent} from './desktop/desktop.component';
@@ -12,9 +12,8 @@ import {ContactComponent} from './widget/contents/contact/contact.component';
 import {CodingComponent} from './widget/contents/coding/coding.component';
 import {CvComponent} from './widget/contents/cv/cv.component';
 import {ImprintComponent} from './widget/contents/imprint/imprint.component';
-import {PixelflutComponent} from './widget/contents/coding/pixelflut/pixelflut.component';
 import {WindowsUpdateComponent} from './widget/contents/windows-update/windows-update.component';
-import {PopupComponent} from "./widget/popup.component";
+import {PopupComponent} from './widget/popup.component';
 
 @NgModule({
     declarations: [
@@ -27,7 +26,6 @@ import {PopupComponent} from "./widget/popup.component";
         CodingComponent,
         CvComponent,
         ImprintComponent,
-        PixelflutComponent,
         WindowsUpdateComponent,
         PopupComponent,
     ],
--- src/app/widget/contents/coding/coding.component.ts
+++ src/app/widget/contents/coding/coding.component.ts
@@ -1,15 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {PixelflutClient} from 'pixelflut-client';
 
 @Component({
-  selector: 'app-coding',
-  templateUrl: './coding.component.html',
-  styleUrls: ['./coding.component.css']
+    selector: 'app-coding',
+    templateUrl: './coding.component.html',
+    styleUrls: ['./coding.component.css']
 })
 export class CodingComponent implements OnInit {
 
-  constructor() { }
+    pxCanvasWidth = -1;
+    pxCanvasHeight = -1;
 
-  ngOnInit() {
-  }
+    private pxClient;
+
+    constructor(private elRef: ElementRef) {
+    }
+
+    ngOnInit(): void {
+        const canvas = this.elRef.nativeElement.getElementsByTagName('canvas')[0];
+        this.pxClient = new PixelflutClient('wss://www.finn-thorben.me/pixelflut', canvas);
+    }
 
 }
