51d0eb8ebde5d24e8e7587c77c3ac794b9ec6b8c
Finn-Thorben Sell <dev@finn-thorben.me>
2020-04-05 14:36:12 +0200

    add vue app

--- src/verysecretriddles/challenge_framework.head.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="/verysecretriddles/resources/spectre.min.css">
-<link rel="stylesheet" href="/verysecretriddles/resources/spectre-exp.min.css">
-<link rel="stylesheet" href="/verysecretriddles/resources/spectre-icons.min.css">
-
-<script src="/verysecretriddles/resources/jquery-3.3.1.min.js"></script>
