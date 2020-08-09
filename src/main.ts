fe2c919a68d12225ec8ee57f724f84f38374493f
Finn-Thorben Sell <dev@finn-thorben.me>
2020-08-09 18:02:55 +0200

    Revert "add matomo tracking"
    This reverts commit a4dc4f3cd62650287732b6cf2a87b5e2327d357f.

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './plugins/router';
 import TaskManager from '@/plugins/taskManager';
-// @ts-ignore
-import VueMatomo from 'vue-matomo';
 
 Vue.config.productionTip = false;
 
 Vue.use(TaskManager);
-
-Vue.use(VueMatomo, {
-  host: "https://analytics.finn-thorben.me/",
-  siteId: 2,
-  trackerFileName: "matomo",
-  router: router,
-  enableLinkTracking: true,
-  requireConsent: false,
-  trackInitialView: true,
-  disableCookies: false,
-  enableHeartBeatTimer: false,
-  debug: false,
-})
 
 new Vue({
   router,
