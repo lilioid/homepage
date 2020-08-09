504255c934ae42103cfa61cb5422c44d8612e27e
Finn-Thorben Sell <dev@finn-thorben.me>
2020-08-09 18:11:55 +0200

    add link to verysecretriddles

--- src/components/content/Coding.vue
+++ src/components/content/Coding.vue
@@ -47,6 +47,13 @@
 
     <div class="shadow-inverse project">
       <h1>Riddles</h1>
+      <p>
+        Hidden on this page is the entry to a few programmatic riddles I have prepared.
+        They are intended for programmers and other technically adept people but anyone can have a
+        go as far as I'm concerned.<br>
+        The most important thing is to have fun!
+      </p>
+      <a v-show="false" href="/verysecretriddles/">Here is the entrance</a>
     </div>
 
     <div class="shadow-inverse project">
