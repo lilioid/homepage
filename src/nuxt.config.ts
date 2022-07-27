e0f6acadb0b6c080ff6aaf4b4d0085664800780d
Finn-Thorben Sell <dev@finn-thorben.me>
2022-07-27 19:51:59 +0200

    add basic nuxt config

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    alias: {
+        "@": "/",
+    },
+    app: {
+        head: {
+            title: "Finn-Thorben Sell"
+        }
+    },
+    telemetry: false,
+    typescript: {
+        strict: true,
+        typeCheck: true,
+    }
 })
