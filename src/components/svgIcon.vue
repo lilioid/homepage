173bbafc7b8bb5bd3bdf0611b31c2b7c22c7d470
Finn-Thorben Sell <dev@finn-thorben.me>
2022-07-28 14:28:56 +0200

    implement svgIcon component

---
+++ src/components/svgIcon.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = withDefaults(
+    defineProps<{
+        size?: number;
+        path: string;
+    }>(),
+    {
+        size: 24,
+    }
+);
+</script>
+
+<template>
+    <svg :width="props.size" :height="props.size" viewBox="0 0 24 24">
+        <path class="fill-current" :d="props.path" />
+    </svg>
+</template>
+
+<style scoped></style>
