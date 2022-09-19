d0fceffad4a480253adcb0465d141aa6501fad10
Finn-Thorben Sell <dev@finn-thorben.me>
2022-09-19 20:48:57 +0200

    implement clock component

---
+++ src/components/clock.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from "#imports";
+import { useIntervalFn } from "@vueuse/core";
+
+let time = ref("00:00:00");
+
+// this is used instead of `useNow()` to avoid hydration mismatch
+// TODO Improve with useNow() somehow
+if (process.client) {
+    useIntervalFn(() => {
+        time.value = new Date().toLocaleTimeString();
+    }, 100);
+}
+</script>
+
+<template>
+    {{ time }}
+</template>
