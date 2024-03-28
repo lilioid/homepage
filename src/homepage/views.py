41e49a32db997e834fc8f3378c3f07a2bdbe997e
ftsell <dev@ftsell.de>
2024-03-28 19:47:04 +0100

    make all urls include .html (because thats funny)

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -1,21 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 from . import templates
 
 router = APIRouter()
 
 
-@router.get("/", tags=["site"])
+@router.get("/")
+async def index_redirect() -> RedirectResponse:
+    return RedirectResponse("/index.html")
+
+
+@router.get("/index.html", tags=["site"])
 async def index(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request, name="views/index.html")
 
 
-@router.get("/projects", tags=["site"])
+@router.get("/projects.html", tags=["site"])
 async def projects(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request, name="views/projects.html")
 
 
-@router.get("/pixelflut", tags=["site"])
+@router.get("/pixelflut.html", tags=["site"])
 async def pixelflut(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request, name="views/pixelflut.html")
