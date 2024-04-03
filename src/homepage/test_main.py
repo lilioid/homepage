1cc0f67f461a802edfe273e5d3ccf944567ce4a5
ftsell <dev@ftsell.de>
2024-04-03 13:21:06 +0200

    fix tests to actually do something

--- src/homepage/test_main.py
+++ src/homepage/test_main.py
@@ -1,41 +1,42 @@
+import logging
 import re
 
-import pytest
 from bs4 import BeautifulSoup
-from fastapi import APIRouter
+from fastapi.routing import APIRoute
 from fastapi.testclient import TestClient
 
 from .main import app
 
+logger = logging.getLogger(__name__)
 client = TestClient(app)
 
-SITE_ROUTES = [
-    route.path
-    for mount in app.routes
-    if isinstance(mount.app, APIRouter)
-    for route in mount.routes
-    if "site" in route.tags
-]
+SITE_ROUTES = [route.path for route in app.routes if isinstance(route, APIRoute) and "site" in route.tags]
 
 
 def test_internal_links():
+    """Validate that internal links are actually resolvable and don't generate 404s"""
     for url in SITE_ROUTES:
+        logger.info(f"Testing site {url}")
         response = client.get(url)
         soup = BeautifulSoup(response.content, features="html.parser")
         for anchor in soup.find_all(name="a"):
             if "external" not in anchor.attrs.get("rel", []):
                 href = anchor.attrs["href"]
+                logger.info(f"Validating internal link to {href}")
                 response = client.get(href, follow_redirects=True)
                 assert response.status_code == 200, f"response for {href} was not OK"
 
 
 def test_external_links():
+    """Validate that all links starting with https:// are also marked as external"""
     for url in SITE_ROUTES:
+        logger.info(f"Testing site {url}")
         response = client.get(url)
         soup = BeautifulSoup(response.content, features="html.parser")
         for anchor in soup.find_all(name="a"):
             href = str(anchor.attrs["href"])
             if re.match(r"https?://", href):
+                logger.info(f"Validating link to {href}")
                 assert "external" in anchor.attrs.get(
                     "rel", []
                 ), f"external link to {href} on site {url} does not have rel=external attribute"
