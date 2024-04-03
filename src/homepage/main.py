32309d9f257bb7b53c8d053ca4338fc01b10797d
ftsell <dev@ftsell.de>
2024-04-03 22:36:56 +0200

    adapt cache times to only use stale content when revalidation errors

--- src/homepage/main.py
+++ src/homepage/main.py
@@ -32,14 +32,14 @@ async def add_cache_headers(request: Request, call_next) -> Response:
         STALE_TIME = 30 * 60 * 60 * 24  # 30 days
         response.headers.setdefault(
             "Cache-Control",
-            f"max-age={FRESH_TIME}, stale-while-revalidate={STALE_TIME}, stale-if-error={STALE_TIME}",
+            f"max-age={FRESH_TIME}, stale-if-error={STALE_TIME}",
         )
     else:
-        FRESH_TIME = 1 * 60  # 1 minute
+        FRESH_TIME = 10 * 60  # 10 minutes
         STALE_TIME = 30 * 60 * 60 * 24  # 30 days
         response.headers.setdefault(
             "Cache-Control",
-            f"max-age={FRESH_TIME}, stale-while-revalidate={STALE_TIME}, stale-if-error={STALE_TIME}",
+            f"max-age={FRESH_TIME}, stale-if-error={STALE_TIME}",
         )
     return response
 
