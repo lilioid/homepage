af8cc28d57646bb1d517e78e2e182e0e10b205bb
ftsell <dev@ftsell.de>
2024-04-03 22:56:41 +0200

    add test for validating links in <head>

--- src/homepage/test_main.py
+++ src/homepage/test_main.py
@@ -40,3 +40,30 @@ def test_external_links():
                 assert "external" in anchor.attrs.get(
                     "rel", []
                 ), f"external link to {href} on site {url} does not have rel=external attribute"
+
+
+def test_head_links():
+    """Validate that all <link> & <script> tags are relative (therefore first-party) and serve valid content"""
+    for url in SITE_ROUTES:
+        logger.info(f"Testing site {url}")
+        response = client.get(url)
+        soup = BeautifulSoup(response.content, features="html.parser")
+
+        for link in soup.find("head").find_all("link"):
+            href = str(link.attrs["href"])
+            if "stylesheet" in link.attrs["rel"] or "preload" in link.attrs["rel"]:
+                assert re.match(
+                    r"^[\\./].+", href
+                ), f"<link> tag references {href} which is not served by this site; external links are forbidden"
+                assert (
+                    client.get(href).status_code == 200
+                ), f"response for referenced link href {href} was not OK"
+
+        for script in soup.find("head").find_all("script"):
+            assert script.text == "", "<script> tag has content; inline scripts are not allowed"
+            src = str(script.attrs["src"])
+            assert re.match(
+                r"^[\\./].+", src
+            ), f"<script> tags source is not served by this site; external scripts are forbidden"
+            assert src.endswith(".mjs"), f"<script> tags source is not a .mjs file"
+            assert client.get(src).status_code == 200, f"response for scripts source {src} was not OK"
