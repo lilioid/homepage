e7a02e0519846dd926be9c196d5b6e4b2eef25ff
ftsell <dev@ftsell.de>
2024-09-24 19:29:43 +0200

    add Freddy as friend

--- src/homepage/friends.py
+++ src/homepage/friends.py
@@ -11,13 +11,14 @@ class Friend:
 
 
 FRIENDS = [
-    Friend(name="Noah Fust", url="https://noahfuhst.de/"),
-    Friend(name="Vic Wrobel", url="https://www.vicwrobel.de/"),
     Friend(name="bonsaibeef.de", url="https://bonsaibeef.de/"),
+    Friend(name="Freddy", url="https://frederico.info/"),
+    Friend(name="Helena", url="https://helena.place/"),
     Friend(name="kritzl", url="https://kritzl.dev/"),
+    Friend(name="Noah Fust", url="https://noahfuhst.de/"),
     Friend(name="olebit", url="https://oleb.it/"),
     Friend(name="traumweh", url="https://traumweh.dev/"),
-    Friend(name="Helena", url="https://helena.place/"),
+    Friend(name="Vic Wrobel", url="https://www.vicwrobel.de/"),
 ]
 
 
