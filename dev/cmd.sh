ffdc8fc3b3ae835274bbd7b7018a26c73488891d
lilly <li@lly.sh>
2025-07-22 16:19:29 +0200

    update container config

--- dev/cmd.sh
+++ dev/cmd.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 set -ex
 
-/usr/local/src/tracker/backend/manage.py migrate
-/usr/local/src/tracker/backend/manage.py check --deploy
+/usr/local/src/homepage/manage.py migrate
+/usr/local/src/homepage/manage.py check --deploy
 
-nginx
-node /usr/local/src/tracker/frontend/.output/server/index.mjs &
-/usr/local/src/tracker/backend/manage.py runserver --noreload --nostatic 0.0.0.0:8001
+/usr/local/src/homepage/manage.py runserver --noreload --nostatic 0.0.0.0:8000
