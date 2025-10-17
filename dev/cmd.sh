19593879cc5b3366ffb9f7ae6457bbe7ec318f2a
lilly <li@lly.sh>
2025-10-17 14:35:54 +0200

    allow args to be passed to the container

--- dev/cmd.sh
+++ dev/cmd.sh
@@ -4,4 +4,4 @@ set -ex
 /usr/local/src/homepage/manage.py migrate
 /usr/local/src/homepage/manage.py check --deploy
 
-/usr/local/src/homepage/manage.py runserver --noreload --nostatic 0.0.0.0:8000
+/usr/local/src/homepage/manage.py runserver --noreload --nostatic $@
